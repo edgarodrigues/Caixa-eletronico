fe97849d5fe85e62423db8ee2e439a70ed9d3f97
Edgar Rodrigues <edgarodgui@gmail.com>
2024-10-10 11:26:26 -0300

    texto

--- Caixa-eletronico/Program.cs
+++ Caixa-eletronico/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System.Diagnostics;
 
 Console.WriteLine("(1) Realizar o depósito do dinheiro");
-
-Console.WriteLine("(2) Realize o saque do dinheiro");
-
-Console.WriteLine("(3) Realize a transferência do dinheiro");
-
-Console.WriteLine("(4) Realize a impressão do extrato");
-
+Console.WriteLine("(2) Realizar o saque do dinheiro");
+Console.WriteLine("(3) Realizar a transferência do dinheiro");
+Console.WriteLine("(4) Realizar a impressão do extrato");
 Console.WriteLine("Sair");
+
+Console.ReadLine("Solicitar o valor a ser depositado");
+Console.ReadLine("Armazenar o valor que a pessoa digitar no teclado em uma variável");
+
+Console.ReadLine("Digitar as informações do indivíduo que pretende sacar dinheiro");
+Console.ReadLine("Retirar a quantidade de dinheiro solicitada");
+
+Console.ReadLine("")
+
 
 
