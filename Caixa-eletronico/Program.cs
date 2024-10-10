89723f1954dd86a22cb33a8624e6e69114200fa1
Edgar Rodrigues <edgarodgui@gmail.com>
2024-10-10 09:52:53 -0300

    Adicionando arquivo

---
+++ Caixa-eletronico/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("(1) Realizar o depósito do dinheiro");
+
+Console.WriteLine("(2) Realize o saque do dinheiro");
+
+Console.WriteLine("(3) Realize a transferência do dinheiro");
+
+Console.WriteLine("(4) Realize a impressão do extrato");
+
+Console.WriteLine("Sair");
+
+
